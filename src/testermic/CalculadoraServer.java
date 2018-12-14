c369b10a5b963f6e10560cbc967f9f453c49a9f2
joimar <joimarbgf@gmail.com>
2018-12-14 00:38:17 -0300

    Exemplo inicial

---
+++ src/testermic/CalculadoraServer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testermic;
+
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+
+/**
+ *
+ * @author joimar
+ */
+public class CalculadoraServer 
+{
+    CalculadoraServer()
+    {
+        try
+        {
+            System.setProperty("java.rmi.server.hostname", "192.168.86.10");
+            LocateRegistry.createRegistry(1099);
+            Calculadora c = new CalculadoraImple();
+            Naming.bind("CalculadoraServer", (Remote) c);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        new CalculadoraServer();
+    }
+}
